e1ef05d3121d227ffae8034298cadbfbf82b4de7
PHoepner <PatrickHoepner@gmail.com>
2021-03-19 18:24:04 -0500

    Create gen_gif.py

---
+++ gen_gif.py
@@ -0,0 +1,14 @@
+from big_sleep import Imagine
+import sys
+
+text = sys.argv[1]
+
+train = Imagine(
+    text = text,
+    lr = 0.07,
+    save_every = 5,
+    save_progress = True,
+    epochs = 1
+)
+
+train()
